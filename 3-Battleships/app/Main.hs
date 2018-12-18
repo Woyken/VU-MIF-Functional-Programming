ce2ea90af1d798658291a5c3933f40c191953a87
Karolis <karolis.uzkuraitis@gmail.com>
2018-12-18 18:08:54 +0200

    3rd assigments starts with a copy from 2nd

---
+++ 3-Battleships/app/Main.hs
@@ -0,0 +1,241 @@
+module Main where
+
+import Lib
+
+
+import Data.Maybe
+import Data.Either
+import Data.Char
+import qualified Data.Map.Strict as Map
+import Data.Map (Map())
+import Control.Exception
+
+import Data.BEncode
+import Data.BEncode.Parser
+import Data.BEncode.Lexer
+
+
+
+{-
+BattleshipsMsg {bsMcoord = Just ('A', 3), bsMresult = Just False, bsMprev = Just BattleshipsMsg {bsMcoord = Just ('B', 4), bsMresult  = Nothing, bsMprev = Nothing}}
+
+
+d
+ 5:coord
+ d
+  1:1 1:F
+  1:2 1:1
+ e
+
+ 4:prev
+ d
+  5:coord
+  d
+   1:1 1:F
+   1:2 1:1
+  e
+ e
+
+ 6:result
+ 4:MISS
+e
+d5:coordde4:prevd5:coordd1:11:F1:21:1ee6:result4:MISSe
+
+*****************************************************************************************************************************************************
+
+d
+ 5:coord
+ d
+  1:1 1:F
+  1:2 1:1
+ e
+
+ 4:prev
+ d
+  5:coord
+  d
+   1:1 1:F
+   1:2 1:1
+  e
+ e
+
+ 6:result
+ 4:MISS
+e
+
+d5:coordd1:11:F1:21:1e4:prevd5:coordd1:11:F1:21:1e6:result4:MISSee
+
+Idea:
+Player A posts first move;
+Player B gets the move, makes it's own, sends back
+...
+
+Need to:
+Place my ships. ------ a way to save all the data. Probably whole table is enough.
+Get move
+Parse bencode, find all moves.
+Add my own move to whole list.
+Post with move
+if all my ships down, send empty string -> defeat.
+
+-------------------
+Turn Bencode to my type and vice versa?
+
+
+------------------------
+    A        B        A       B        A
+A2
+A2:false;A1
+A2:false;A1:true;A1
+A2:false;A1:true;A1:false;B2
+A2:false;A1:true;A1:false;B2:false;C3
+...
+
+
+-}
+
+{-
+
+Generate ships.
+Move command.
+Get/Post
+-}
+
+
+
+-- 5 ships T formation.
+
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- |  |A|B|C|D|E|F|G|H|I|J|    |  |A|B|C|D|E|F|G|H|I|J|
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 1|x|x|x| |x|x|x| | | |    | 1| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 2| |x| | | |x| | | | |    | 2|?| | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 3| | | |x| | | |x| | |    | 3| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 4| | | |x|x| |x|x|x| |    | 4| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 5| | | |x| | | | | | |    | 5| |*| | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 6| | |x| | | | | | | |    | 6| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 7| |x|x| | | | | | | |    | 7| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 8| | |x| | | | | | | |    | 8| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- | 9| | | | | | | | | | |    | 9| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+-- |10| | | | | | | | | | |    |10| | | | | | | | | | |
+-- +--+-+-+-+-+-+-+-+-+-+-+    +--+-+-+-+-+-+-+-+-+-+-+
+
+-- A1,B1,C1,B2, E1,F1,G1,F2, D3,D4,E4,D5, H3,G4,H4,I4, C6,B7,C7,C8
+-- ('A', 1),('B', 1),('C', 1),('B', 2), ('E', 1),('F', 1),('G', 1),('F', 2), ('D', 3),('D', 4),('E', 4),('D', 5), ('H', 3),('G', 4),('H', 4),('I', 4), ('C', 6),('B', 7),('C', 7),('C', 8)
+_MY_SHIPS_ON_TABLE = BattleshipsTable {bsTentries = [('A', 1),('B', 1),('C', 1),('B', 2), ('E', 1),('F', 1),('G', 1),('F', 2), ('D', 3),('D', 4),('E', 4),('D', 5), ('H', 3),('G', 4),('H', 4),('I', 4), ('C', 6),('B', 7),('C', 7),('C', 8)]}
+
+_DEFINED_CONTENT_TYPE = "application/bencoding+nolists"
+
+getRequestUrl :: String -> String -> String
+getRequestUrl gameServerName playerId = "http://battleship.haskell.lt/game/" ++ gameServerName ++ "/player/" ++ playerId
+
+main :: IO ()
+main = do
+    putStrLn "PlayerID (A/B):"
+    playerId <- getLine
+    if (playerId /= "A" && playerId /= "B") then
+        putStrLn $ "Invalid Player ID: " ++ playerId
+    else do
+        putStrLn "GameServer name:"
+        gameServerName <- getLine
+        if (length gameServerName < 1) then
+            putStrLn "GameServer name invalid"
+        else do
+            if (playerId == "A") then do
+                let bmsg = BattleshipsMsg {bsMcoord = Just ('A', 1), bsMresult = Nothing, bsMprev = Nothing}
+                let toSend = bShow (moveToBEncode bmsg) ""
+                postResponseStr <- sendPostRequest toSend (getRequestUrl gameServerName playerId) _DEFINED_CONTENT_TYPE
+                putStrLn $ "Starting the game. Response: " ++ postResponseStr
+                mainMoves gameServerName playerId
+            else do
+                mainMoves gameServerName playerId
+
+parserExceptionHandler :: SomeException -> IO (Either String BattleshipsMsg)
+parserExceptionHandler _ = return (Left ("BEncode library Has thrown an exception"))
+
+mainMoves :: String -> String -> IO ()
+mainMoves gameServerName playerId = do
+    gotBencode <- getTheMovesRequest (getRequestUrl gameServerName playerId) _DEFINED_CONTENT_TYPE
+    -- parsedBsMoves <- (return (parseAllMoves gotBencode)) `catch` \e -> return (Left ("Caught " ++ show (e :: SomeException)))
+    parsedBsMoves <- catch (return (parseAllMoves gotBencode)) parserExceptionHandler
+    if (isLeft parsedBsMoves) then do
+        -- Can't parse Bencode, can't continue!.
+        putStrLn (fromLeft "" parsedBsMoves)
+        -- For now treat this as a win, parser library fails when there's an empty list...
+        putStrLn "I won, I guess... Okay, this has to be a bug in parser library."
+    else do
+        let parsedBsMovesR = fromRight (BattleshipsMsg {bsMcoord = Nothing, bsMresult = Nothing, bsMprev = Nothing}) parsedBsMoves
+        if (checkMyWin parsedBsMovesR) then
+            putStrLn "I won!"
+        else do
+            let parsedBsMovesFilled = fillOponentsResult _MY_SHIPS_ON_TABLE parsedBsMovesR
+            let enemyPlayerMoves = selectOnlyPlayerMoves (getEnemyPlayerId playerId) parsedBsMovesFilled
+            let meAlive = isNothing enemyPlayerMoves || checkMeAlive _MY_SHIPS_ON_TABLE (fromJust enemyPlayerMoves)
+            if (meAlive == False) then do
+                -- add empty dict and send... I lost...
+                let toSend = bShow (moveToBEncode $ parsedBsMovesFilled) ""
+                postResponseStr <- sendPostRequest toSend (getRequestUrl gameServerName playerId) _DEFINED_CONTENT_TYPE
+                putStrLn $ "I lost... Sending loosing message... response: " ++ postResponseStr
+            else do
+                -- add my own move.
+                let nextMove = checkAllBsMMoves (fromJust (selectOnlyPlayerMoves (playerId) (parsedBsMovesFilled))) ('A', 1)
+                if (isLeft nextMove) then do
+                    -- nowhere to move anymore...
+                    -- Send surrender message?
+                    let toSend = bShow (moveToBEncode $ parsedBsMovesFilled) ""
+                    postResponseStr <- sendPostRequest toSend (getRequestUrl gameServerName playerId) _DEFINED_CONTENT_TYPE
+                    putStrLn $ "Next move position was not found. Can't continue, sending loss message. response: " ++ postResponseStr
+                else do
+                    let nextMoveR = fromRight ('A', 1) nextMove
+                    let toSend = bShow (moveToBEncode $ addMyMove parsedBsMovesFilled nextMoveR) ""
+                    postResponseStr <- sendPostRequest toSend (getRequestUrl gameServerName playerId) _DEFINED_CONTENT_TYPE
+                    putStrLn toSend
+                    putStrLn $ "Made a move, sending... response: " ++ postResponseStr
+                    mainMoves gameServerName playerId
+
+
+
+
+
+
+
+
+
+
+
+
+{-
+parseBencodeToMyType :: String -> BattleshipsMsg
+parseBencodeToMyType msg = do
+    return bsTentries [[]]
+
+-}
+    {-
+
+parseBencodeToMyTypeInternal :: BEncode -> BattleshipsMsg
+parseBencodeToMyTypeInternal benc = do
+    putStrLn "Not implemented"
+    -}
+
+
+{-
+http://hackage.haskell.org/package/bencode-0.6.0.0/docs/Data-BEncode.html
+http://hackage.haskell.org/package/bencode-0.6.0.0/docs/Data-BEncode-Parser.html
+-}
+{-
+Get value from dictionary.
+fromRight (BInt 5) ( runParser (dict "1") (fromJust (bRead (Bsl.fromStrict (Bs.pack "d1:11:A1:21:4e")))))
+-}
+{-
+Get string!
+runParser (bstring token) (parseBencode "5:coord")
+-}
---
+++ 3-Battleships/src/Lib.hs
@@ -0,0 +1,327 @@
+module Lib
+    ( getTheMovesRequest,
+    sendPostRequest,
+    BattleshipsMsg (..),
+    BattleshipsTable (..),
+    addMyMove,
+    moveToBEncode,
+    checkAllBsMMoves,
+    checkMeAlive,
+    getEnemyPlayerId,
+    selectOnlyPlayerMoves,
+    fillOponentsResult,
+    checkMyWin,
+    parseAllMoves
+    ) where
+
+import Network.HTTP
+import Network.HTTP.Headers
+import Network.Stream (Result)
+import Data.Maybe
+import Data.Either
+import Data.Char
+import qualified Data.Map.Strict as Map
+import Data.Map (Map())
+import Control.Exception
+
+import Data.BEncode
+import Data.BEncode.Parser
+import Data.BEncode.Lexer
+import qualified Data.ByteString.Lazy as Bsl
+import qualified Data.ByteString.Char8 as Bs
+
+data BattleshipsMsg = BattleshipsMsg {
+    bsMcoord :: Maybe (Char, Int),
+    bsMresult :: Maybe Bool,
+    bsMprev :: Maybe BattleshipsMsg
+} deriving Show
+
+data BattleshipsTable = BattleshipsTable {
+    bsTentries :: [(Char, Int)]
+} deriving Show
+
+--sendGetRequest :: IO String
+--sendGetRequest = simpleHTTP (getRequest _DEFINED_REQUEST_URL) >>= getResponseBody
+
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+getTheMovesRequest :: String -> String -> IO String
+getTheMovesRequest requestUrl requestContentType = do
+    response <- simpleHTTP ((getRequest requestUrl){ rqHeaders = [Header (HdrAccept) requestContentType] })
+    responseValue <- (getResponseBody response)
+    responseCode <- getResponseCode response
+    let responseCodeFirstNum = fst3 (responseCode)
+    if (responseCodeFirstNum == 2) then do
+        return responseValue
+    else if (responseCodeFirstNum == 4) then do
+        putStrLn "No move available yet. Trying again..."
+        getTheMovesRequest requestUrl requestContentType
+    else do
+        putStrLn ("Unexpected response from server, retrying... " ++ responseValue)
+        getTheMovesRequest requestUrl requestContentType
+
+sendPostRequest :: String -> String -> String -> IO String
+sendPostRequest move requestUrl requestContentType = simpleHTTP (postRequestWithBody (requestUrl) requestContentType move) >>= getResponseBody
+
+--sendPostRequestAsEnemy :: String -> IO String
+--sendPostRequestAsEnemy move = simpleHTTP (postRequestWithBody ("http://battleship.haskell.lt/game/" ++ _DEFINED_GAMESERVER_NAME ++ "/player/" ++ getEnemyPlayerId) _DEFINED_CONTENT_TYPE move) >>= getResponseBody
+
+printBEncode :: BEncode -> IO ()
+printBEncode s = do
+    putStrLn (bShow s "")
+
+addMyMove :: BattleshipsMsg -> (Char, Int) -> BattleshipsMsg
+addMyMove fullList myShotCoords = BattleshipsMsg {bsMcoord = Just myShotCoords, bsMresult = bsMresult fullList, bsMprev = bsMprev fullList}
+
+checkMyWin :: BattleshipsMsg -> Bool
+checkMyWin msg = isNothing $ bsMcoord msg
+
+getEnemyPlayerId :: String -> String
+getEnemyPlayerId plId
+    | plId == "A" = "B"
+    | True = "A"
+
+
+fillOponentsResult :: BattleshipsTable -> BattleshipsMsg -> BattleshipsMsg
+fillOponentsResult table msg = do
+    let shotCoords = bsMcoord msg
+    if (isNothing shotCoords) then
+        -- Seems like we have won.
+        msg
+    else do
+        BattleshipsMsg {bsMcoord = Nothing, bsMresult = Just (didShotHit table (fromJust shotCoords)), bsMprev = Just msg}
+
+-- region FROM FIRST PROGRAM
+{-
+Gets next available coordinate
+-}
+getNextCoord :: (Char, Int) -> Either String (Char, Int)
+getNextCoord (letter, number)
+  | ord letter >= ord 'J' && number >= 10 = Left "Board is full"
+  | ord letter >= ord 'J' = Right ('A', number + 1)
+  | ord letter < ord 'J' = Right (chr (ord letter + 1), number)
+  | True = Left $ "Something went wrong while getting new coord." ++ ([letter] ++ show number)
+
+{-
+Check if coordinate aleady exists in current message
+-}
+checkIfCoordConflicts :: BattleshipsMsg -> (Char, Int) -> Bool
+checkIfCoordConflicts bsM (letter, number)
+  | isJust (bsMcoord bsM) == True && snd (fromJust (bsMcoord bsM)) == number && fst (fromJust (bsMcoord bsM)) == letter = True
+  | True = False
+
+{-
+Check if with this coordinate, player has any spots.
+-}
+checkBsMMove :: BattleshipsMsg -> (Char, Int) -> Bool
+checkBsMMove bsM (letter, number)
+  | checkIfCoordConflicts bsM (letter, number) == True = True
+  | isJust (bsMprev bsM) = checkBsMMove (fromJust (bsMprev bsM)) (letter, number)
+  | True = False
+
+{-
+Iterate through all coordinates for the player
+-}
+checkAllBsMMoves :: BattleshipsMsg -> (Char, Int) -> Either String (Char, Int)
+checkAllBsMMoves bsM (letter, number)
+  | checkBsMMove bsM (letter, number) =
+    let
+      nextCoord = getNextCoord (letter, number)
+      result =
+        if isLeft nextCoord then
+          Left (fromLeft "Something went horribly wrong, isLeft && fromLeft failed!" nextCoord)
+        else
+          checkAllBsMMoves bsM (fromRight ('A', 1) nextCoord)
+    in
+      result
+  | True = Right (letter, number)
+
+-- end region FROM FIRST PROGRAM
+
+didShotHit :: BattleshipsTable -> (Char, Int) -> Bool
+didShotHit table targetCoord = do
+    let tableEntries = bsTentries table
+    if (null tableEntries) then
+        False
+    else do
+        let cur = head tableEntries
+        if (areCoordsEqual cur targetCoord) then
+            True
+        else do
+            didShotHit (BattleshipsTable {bsTentries = tail tableEntries}) targetCoord
+
+checkMeAlive :: BattleshipsTable -> BattleshipsMsg -> Bool
+checkMeAlive table attackerMsg = do
+    -- iterate table and check if that coord was shot already
+    let tableEntries = bsTentries table
+    -- Iteration has reached the end of list. That means I lost.
+    if (null tableEntries) then
+        False
+    else do
+        let cur = head tableEntries
+        -- if any of my ships still exists, I'm alive!
+        if (checkCoordShot cur attackerMsg == False) then
+            True
+        else do
+            checkMeAlive (BattleshipsTable {bsTentries = tail tableEntries}) attackerMsg
+
+areCoordsEqual :: (Char, Int) -> (Char, Int) -> Bool
+areCoordsEqual (c1, i1) (c2, i2) = c1 == c2 && i1 == i2
+
+checkCoordShot :: (Char, Int) -> BattleshipsMsg -> Bool
+checkCoordShot (ch, i) msg = do
+    let currentCoord = bsMcoord msg
+    if (isJust currentCoord && areCoordsEqual (ch, i) (fromJust currentCoord)) then
+        True
+    else do
+        let prevMove = bsMprev msg
+        if (isJust prevMove) then
+            checkCoordShot (ch, i) (fromJust prevMove)
+        else
+            False
+
+checkMoveBelongsToPlayerA :: BattleshipsMsg -> Bool
+checkMoveBelongsToPlayerA msg = do
+    let prevMove = bsMprev msg
+    if (isNothing prevMove) then
+        True
+    else do
+        let prevPrevMove = bsMprev (fromJust prevMove)
+        if (isNothing prevPrevMove) then
+            False
+        else
+            checkMoveBelongsToPlayerA (fromJust prevPrevMove)
+
+-- If I am 'A' then should check all 'B' moves when I am dead.
+-- Copied 2ice, could refactor.........
+selectOnlyPlayerMoves :: [Char] -> BattleshipsMsg -> Maybe BattleshipsMsg
+selectOnlyPlayerMoves "A" msg = do
+    if (checkMoveBelongsToPlayerA msg) then do
+        let prevMove = bsMprev msg
+        if (isNothing prevMove) then
+            Just BattleshipsMsg {bsMcoord = bsMcoord msg, bsMresult = bsMresult msg, bsMprev = Nothing}
+        else do
+            Just BattleshipsMsg {bsMcoord = bsMcoord msg, bsMresult = bsMresult msg, bsMprev = selectOnlyPlayerMoves "A" (fromJust prevMove)}
+    else do
+        let prevMove = bsMprev msg
+        if (isNothing prevMove) then
+            -- This shouldn't be possible...
+            Just BattleshipsMsg {bsMcoord = Nothing, bsMresult = Nothing, bsMprev = Nothing}
+        else do
+            selectOnlyPlayerMoves "A" (fromJust prevMove)
+selectOnlyPlayerMoves "B" msg = do
+    if (checkMoveBelongsToPlayerA msg == False) then do
+        let prevMove = bsMprev msg
+        if (isNothing prevMove) then
+            -- This shouldn't be possible...
+            Just BattleshipsMsg {bsMcoord = Nothing, bsMresult = Nothing, bsMprev = Nothing}
+        else do
+            Just BattleshipsMsg {bsMcoord = bsMcoord msg, bsMresult = bsMresult msg, bsMprev = selectOnlyPlayerMoves "B" (fromJust prevMove)}
+    else do
+        let prevMove = bsMprev msg
+        if (isNothing prevMove) then
+            Nothing
+        else do
+            selectOnlyPlayerMoves "B" (fromJust prevMove)
+
+-- region Convert To BEncode
+coordToBEncode :: Maybe (Char, Int) -> (String, BEncode)
+coordToBEncode (Just (ch, i)) = ("coord", BDict (Map.fromList [("1", BString (Bsl.fromStrict (Bs.pack [ch]))), ("2", BString (Bsl.fromStrict (Bs.pack (show i))))]))
+coordToBEncode Nothing = ("coord", BDict (Map.fromList []))
+
+resultToBEncode :: Bool -> (String, BEncode)
+resultToBEncode b
+    | b == True  = ("result", BString (Bsl.fromStrict (Bs.pack "HIT")))
+    | b == False = ("result", BString (Bsl.fromStrict (Bs.pack "MISS")))
+
+moveToBEncode :: BattleshipsMsg -> BEncode
+moveToBEncode msg = do
+    let resultVal = bsMresult msg
+    if (isJust resultVal) then do
+        let resultBe = resultToBEncode (fromJust resultVal)
+        let coordVal = bsMcoord msg
+        let coordBe = coordToBEncode coordVal
+        let prevVal = bsMprev msg
+        if(isJust prevVal) then do
+            let prevBe = moveToBEncode (fromJust prevVal)
+            BDict (Map.fromList [coordBe, ("prev", prevBe), resultBe])
+        else do
+            BDict (Map.fromList [coordBe, resultBe])
+    else do
+        let coordVal = bsMcoord msg
+        let coordBe = coordToBEncode coordVal
+        let prevVal = bsMprev msg
+        if(isJust prevVal) then do
+            let prevBe = moveToBEncode (fromJust prevVal)
+            BDict (Map.fromList [coordBe, ("prev", prevBe)])
+        else do
+            BDict (Map.fromList [coordBe])
+
+-- end region Convert To BEncode
+
+-- region Parse From BEncode
+
+note :: Maybe a -> e -> Either e a
+note Nothing e = Left e
+note (Just a) _ = Right a
+
+parseBencode :: String -> Either String BEncode
+parseBencode str = note (bRead (Bsl.fromStrict (Bs.pack str))) "BEncode parser has failed. Probably invalid BEncode format"
+
+parseCoordDict :: BEncode -> Either String (Maybe (Char, Int))
+parseCoordDict dictionary = do
+    let parsingCoordResult = runParser (dict "coord") (dictionary)
+    if (isLeft parsingCoordResult) then
+        return Nothing
+    else do
+        coordLetterAsBeStr <- runParser (dict "1") (fromRight (BInt 0) parsingCoordResult)
+        coordLetterAsStr <- runParser (bstring token) coordLetterAsBeStr
+        let coordChar = head coordLetterAsStr
+        if (ord coordChar > ord 'J' || ord coordChar < ord 'A') then
+            Left "Invalid coordinate letter!"
+        else do
+            coordNumberAsBeStr <- runParser (dict "2") (fromRight (BInt 0) parsingCoordResult)
+            coordNumberAsStr <- runParser (bstring token) coordNumberAsBeStr
+            let numberAsInt = read coordNumberAsStr
+            if (numberAsInt < 1 || numberAsInt > 10) then
+                Left "Invalid coordinate number found!"
+            else do
+                return (Just ((head coordLetterAsStr), numberAsInt))
+
+
+parsePrevDict dictionary = runParser (dict "prev") (dictionary)
+
+parseResultDict :: BEncode -> Either String (Maybe Bool)
+parseResultDict dictionary = do
+    let parsingResultResult = runParser (dict "result") (dictionary)
+    if (isLeft parsingResultResult) then
+        return Nothing
+    else do
+        resultStr <- runParser (bstring token) (fromRight (BInt 0) parsingResultResult)
+        if (resultStr == "MISS") then
+            return (Just False)
+        else if (resultStr == "HIT") then
+            return (Just True)
+        else do
+            Left "Invalid result value"
+
+parseAMove :: BEncode -> Either String BattleshipsMsg
+parseAMove bencode = do
+    coords <- parseCoordDict bencode
+    result <- parseResultDict bencode
+    let parsingPrevResult = parsePrevDict bencode
+    if (isLeft parsingPrevResult) then
+        return BattleshipsMsg {bsMcoord = coords, bsMresult = result, bsMprev = Nothing}
+    else do
+        prevValue <- parseAMove (fromRight (BInt 0) parsingPrevResult)
+        return BattleshipsMsg {bsMcoord = coords, bsMresult = result, bsMprev = Just prevValue}
+
+parseAllMoves :: String -> Either String BattleshipsMsg
+parseAllMoves msg = do
+    allBencode <- parseBencode msg
+    parsed <- parseAMove allBencode
+    return parsed
+
+-- end region Parse From BEncode
+
